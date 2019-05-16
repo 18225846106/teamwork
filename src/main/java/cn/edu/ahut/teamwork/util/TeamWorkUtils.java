0aa09dc1c7a534a5496dba45051ddffae5541be2
aiqingliu <1609115435@qq.com>
2019-05-16 11:04:05 +0800

    分组排序工具方法

--- src/main/java/cn/edu/ahut/teamwork/util/TeamWorkUtils.java
+++ src/main/java/cn/edu/ahut/teamwork/util/TeamWorkUtils.java
@@ -1,7 +1,11 @@
 package cn.edu.ahut.teamwork.util;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import cn.edu.ahut.teamwork.entity.Student;
 
 public class TeamWorkUtils {
 
@@ -27,4 +31,30 @@ public class TeamWorkUtils {
 		}
 		return date;
 	}
+	
+	/**
+	 * 分组,对学生列表重新排序
+	 * @param count 每组多少人
+	 * @param students 学生列表
+	 * @return List<Student> groupstudent
+	 */
+	public List<Student> creatteam(int count,List<Student> students){
+		//分几个小组
+		int groupcount = students.size()/count;
+		if (students.size()%count > 0) {
+			groupcount = groupcount + 1;
+		}
+		//存储排序的students
+		List<Student> groupstudent = new ArrayList<Student>();
+		int j = 0;
+		for(int i=0; i<students.size(); i++) {
+			groupstudent.add(students.get(j));
+			j = j + count;
+			if (j > students.size()) {
+				j = j % students.size();
+			}
+		}
+		return groupstudent;
+	}
+	
 }
