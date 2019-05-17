1104815a659c943aaade216eec4e65d49f80d86d
aiqingliu <1609115435@qq.com>
2019-05-17 09:42:02 +0800

    分组服务组件

--- src/main/java/cn/edu/ahut/teamwork/util/TeamWorkUtils.java
+++ src/main/java/cn/edu/ahut/teamwork/util/TeamWorkUtils.java
@@ -3,7 +3,9 @@ package cn.edu.ahut.teamwork.util;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.edu.ahut.teamwork.entity.Student;
 
@@ -38,7 +40,7 @@ public class TeamWorkUtils {
 	 * @param students 学生列表
 	 * @return List<Student> groupstudent
 	 */
-	public List<Student> creatteam(int count,List<Student> students){
+	public List<Student> sortStudents(Integer count,List<Student> students){
 		//分几个小组
 		int groupcount = students.size()/count;
 		if (students.size()%count > 0) {
@@ -47,14 +49,49 @@ public class TeamWorkUtils {
 		//存储排序的students
 		List<Student> groupstudent = new ArrayList<Student>();
 		int j = 0;
+		int k = 0;
 		for(int i=0; i<students.size(); i++) {
 			groupstudent.add(students.get(j));
-			j = j + count;
-			if (j > students.size()) {
-				j = j % students.size();
+			j = j + groupcount;
+			if (j >= students.size()) {
+				k = k + 1;
+				j = k;
 			}
 		}
 		return groupstudent;
+	}
+	
+	public Map<String, Object> sortStudentsm(Integer count,List<Student> students){
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			
+				//分几个小组
+				int groupcount = students.size()/count;
+				if (students.size()%count > 0) {
+					groupcount = groupcount + 1;
+				}
+				//存储排序的students
+				List<Student> groupstudent = new ArrayList<Student>();
+				//排序的下标
+				List<Integer> groupindex = new ArrayList<Integer>();
+				int j = 0;
+				int k = 0;
+				for(int i=0; i<students.size(); i++) {
+					groupstudent.add(students.get(j));
+					j = j + groupcount;
+					if (j >= students.size()) {
+						k = k + 1;
+						j = k;
+						groupindex.add(groupstudent.size());
+					}
+				}
+				result.put("students", groupstudent);
+				result.put("index", groupindex);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 }
